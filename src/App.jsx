7b25c411181c8c5e3d074a4861ebf192c8addbca
farroshsy <farrossyafei.205025@mhs.its.ac.id>
2022-12-30 15:07:29 +0700

    Test

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import SceneInit from './lib/SceneInit';
+
+function App() {
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.renderer.setClearColor(0xeeeeee, 1);
+    test.animate();
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./assets/piano_bench/scene.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+      gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
+    });
+
+    const animate = () => {
+      if (loadedModel) {
+        loadedModel.scene.rotation.x += 0.01;
+        loadedModel.scene.rotation.y += 0.01;
+        loadedModel.scene.rotation.z += 0.01;
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
+  }, []);
+
+  return (
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  );
+}
+
+export default App;
