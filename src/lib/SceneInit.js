2903757799121ba980fd392f6c27dcd298c7fa97
farroshsy <farrossyafei.205025@mhs.its.ac.id>
2022-12-30 16:14:25 +0700

    testlagi

--- src/lib/SceneInit.js
+++ src/lib/SceneInit.js
@@ -4,23 +4,19 @@ import Stats from 'three/examples/jsm/libs/stats.module';
 
 export default class SceneInit {
   constructor(canvasId) {
-    // NOTE: Core components to initialize Three.js app.
     this.scene = undefined;
     this.camera = undefined;
     this.renderer = undefined;
 
-    // NOTE: Camera params;
     this.fov = 45;
     this.nearPlane = 1;
     this.farPlane = 1000;
     this.canvasId = canvasId;
 
-    // NOTE: Additional components.
     this.clock = undefined;
     this.stats = undefined;
     this.controls = undefined;
 
-    // NOTE: Lighting is basically required.
     this.ambientLight = undefined;
     this.directionalLight = undefined;
   }
@@ -35,15 +31,12 @@ export default class SceneInit {
     );
     this.camera.position.z = 48;
 
-    // NOTE: Specify a canvas which is already created in the HTML.
     const canvas = document.getElementById(this.canvasId);
     this.renderer = new THREE.WebGLRenderer({
       canvas,
-      // NOTE: Anti-aliasing smooths out the edges.
       antialias: true,
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    // this.renderer.shadowMap.enabled = true;
     document.body.appendChild(this.renderer.domElement);
 
     this.clock = new THREE.Clock();
@@ -51,35 +44,19 @@ export default class SceneInit {
     this.stats = Stats();
     document.body.appendChild(this.stats.dom);
 
-    // ambient light which is for the whole scene
     this.ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
     this.ambientLight.castShadow = true;
     this.scene.add(this.ambientLight);
 
-    // directional light - parallel sun rays
     this.directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-    // this.directionalLight.castShadow = true;
     this.directionalLight.position.set(0, 32, 64);
     this.scene.add(this.directionalLight);
 
-    // if window resizes
     window.addEventListener('resize', () => this.onWindowResize(), false);
-
-    // NOTE: Load space background.
-    // this.loader = new THREE.TextureLoader();
-    // this.scene.background = this.loader.load('./pics/space.jpeg');
-
-    // NOTE: Declare uniforms to pass into glsl shaders.
-    // this.uniforms = {
-    //   u_time: { type: 'f', value: 1.0 },
-    //   colorB: { type: 'vec3', value: new THREE.Color(0xfff000) },
-    //   colorA: { type: 'vec3', value: new THREE.Color(0xffffff) },
-    // };
   }
 
   animate() {
-    // NOTE: Window is implied.
-    // requestAnimationFrame(this.animate.bind(this));
+
     window.requestAnimationFrame(this.animate.bind(this));
     this.render();
     this.stats.update();
@@ -87,8 +64,7 @@ export default class SceneInit {
   }
 
   render() {
-    // NOTE: Update uniform data on each render.
-    // this.uniforms.u_time.value += this.clock.getDelta();
+
     this.renderer.render(this.scene, this.camera);
   }
 
